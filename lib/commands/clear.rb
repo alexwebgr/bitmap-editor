d2905244c07d03ddeed0c1ac29b838bd571bf168
alexwebgr <websurfer992@gmail.com>
2020-01-21 14:29:58 +0000

    updated clear command

--- lib/commands/clear.rb
+++ lib/commands/clear.rb
@@ -5,7 +5,7 @@ module AWB
       def self.draw(params)
         matrix = File.read(AWB::OUTPUT_FILE)
         File.open(AWB::OUTPUT_FILE, 'w') do |f|
-          f.write matrix.gsub(/^O/, 'O')
+          f.write matrix.gsub(/[^O\n]/, 'O')
         end
       end
     end
